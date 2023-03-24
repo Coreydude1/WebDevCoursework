bf447f8f90510fd2d9ae74d560fc5d5993f759f6
Coreydude1 <115724761+Coreydude1@users.noreply.github.com>
2023-03-24 10:59:03 +0000

    Add files via upload

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,13 +1,18 @@
 function send_form(id, text){
+    //Changes html id to have updated text
     document.getElementById(id).innerHTML=text;
 }
 
+//logged In is used to track if the user is logged in
 var loggedIn = false;
 
 function attemptLogin(emailId, passwordId){
+    //Checks whether the user is currently logged in
     if(!loggedIn){
+        //Gets the email the user has entered
         let email = document.getElementById(emailId).value;
 
+        //If the email is blank then displays a welcome message else displays a warning
         if(!email == ""){
             alert("Successful Login.\n\nWelcome :  " + email);
             loggedIn = !loggedIn;
@@ -16,6 +21,7 @@ function attemptLogin(emailId, passwordId){
         }
 
     } else {
+        //Logs the user out
         loggedIn = !loggedIn;
         alert("Successfully Logged Out.")
     }
